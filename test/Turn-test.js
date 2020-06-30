ceab9a48f9965888ada8b7b580d1a4a809894228
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-06-29 21:06:08 -0600

    Add properties and methods to Turn class and test for Turn to be a function.

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,8 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-// const Card = require('../src/Card');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 
 describe('Turn', function() {
+
+  it('should be a function', function() {
+    const card = new Card();
+    const turn = new Turn();
+
+    expect(Turn).to.be.a('function');
+  });
 
 });
