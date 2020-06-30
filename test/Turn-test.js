cd2354fdef4867272b32bf3389d7faf8382d737e
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-06-30 11:37:40 -0600

    Add tests to Turn-test.js for returnGuess and returnCard.

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -13,4 +13,28 @@ describe('Turn', function() {
     expect(Turn).to.be.a('function');
   });
 
+  it('should store a guess', function(){
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.guess).to.equal('object');
+  });
+
+  it('should be able to return a guess', function(){
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.returnGuess()).to.equal('object');
+  });
+
+  it('should be able to return a card', function(){
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.returnCard()).to.equal(card);
+  });
+
 });
+
+
+// });
