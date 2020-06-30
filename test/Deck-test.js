39e8700dc47cd0fd76a32b163136f54a3624ee6a
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-06-30 12:19:43 -0600

    Add test for Deck being a function and make it pass.

---
+++ test/Deck-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Card');
+
+describe('Deck', function() {
+
+  it('should be a function', function() {
+    const deck = new Deck();
+
+    expect(Deck).to.be.a('function');
+  });
+
+});
