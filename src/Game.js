d4916b45162c152ae8b59d481cab0ada6dbeefa3
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-02 13:16:35 -0600

    Add start function to Game.js to start the game.

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,26 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+
 class Game {
-  constructor() {}
+  constructor(round) {
+    this.round = 0;
+  }
+
+  start() {
+    let cardsCreated = [];
+    for (var i = 0; i < prototypeQuestions.length; i++) {
+      let card = new Card(prototypeQuestions[i]);
+      cardsCreated.push(card);
+    }
+    const deck = new Deck(cardsCreated)
+    this.round = new Round(deck)
+    this.printMessage(deck, this.round)
+    this.printQuestion(this.round);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
