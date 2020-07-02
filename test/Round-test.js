c551b23ec08d4688f7568cd20b4350897862ed92
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-01 21:09:51 -0600

    Move setup to before in Round-test.js.

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,22 +7,30 @@ const Deck = require('../src/Deck');
 const data = require('../src/data').prototypeData;
 
 describe('Round', function() {
+  var card1;
+  var card2;
+  var card3;
+  var cardsCreated;
+  var deck;
+  var round;
+
+  before(function() {
+    card1 = new Card(data[0]);
+    card2 = new Card(data[1]);
+    card3 = new Card(data[2]);
+
+    cardsCreated = [card1, card2, card3];
+
+    deck = new Deck(cardsCreated);
+
+    round = new Round(deck);
+  })
 
   it('should be a function', function() {
     expect(Round).to.be.a('function');
   });
 
   it('should recognize the current card', function() {
-    const card1 = new Card(data[0]);
-    const card2 = new Card(data[1]);
-    const card3 = new Card(data[2]);
-
-    const cardsCreated = [card1, card2, card3];
-
-    const deck = new Deck(cardsCreated);
-
-    const round = new Round(deck);
-
     expect(round.currentCard).to.equal(card1);
   });
 
