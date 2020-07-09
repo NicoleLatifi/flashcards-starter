5a2ea31457d56be9ac07f260e39ec60aa5d37c5f
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-08 22:00:47 -0600

    Change parameters in Card class to use deconstruction.

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,9 @@
 class Card {
-  constructor(cardDetails) {
-    this.id = cardDetails["id"];
-    this.question = cardDetails["question"];
-    this.answers = cardDetails["answers"];
-    this.correctAnswer = cardDetails["correctAnswer"];
+  constructor({ id, question, answers, correctAnswer }) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
   }
 }
 
